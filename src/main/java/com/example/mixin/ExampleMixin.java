6f1ff03eeba3df4cdcc08c94b2066b4a57b6e7c8
rogatmm-arch <rogatmm@gmail.com>
2026-04-23 20:16:55 +0200

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,18 @@
-package com.example.mixin;
+package com.ft.visuals.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+@Mixin(Entity.class)
+public class EntityMixin {
+    @Inject(method = "isGlowing", at = @At("HEAD"), cancellable = true)
+    private void onIsGlowing(CallbackInfoReturnable<Boolean> cir) {
+        if ((Object) this instanceof PlayerEntity) {
+            cir.setReturnValue(true); // Гравці завжди світяться
+        }
+    }
 }
