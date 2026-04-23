135cab57de904dc02afed40415d3b667d17ba45b
rogatmm-arch <rogatmm@gmail.com>
2026-04-23 20:17:19 +0200

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,15 @@
-package com.example;
+package com.ft.visuals.mixin;
 
-import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.render.LightmapTextureManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+@Mixin(LightmapTextureManager.class)
+public class LightmapMixin {
+    @Inject(at = @At("HEAD"), method = "getBrightness", cancellable = true)
+    private static void onGetBrightness(CallbackInfoReturnable<Float> info) {
+        info.setReturnValue(100.0f); // Встановлюємо яскравість на максимум
+    }
 }
